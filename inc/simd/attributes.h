3d4d50e063473fc7b17adb1f3d57877c331db3d8
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-05-17 12:21:47 +0400

    Redefinition checks in attributes.h
    Change-Id: Ie722c80fbba4b0ce9183d5f0e737ee2c0e389bfb

--- inc/simd/attributes.h
+++ inc/simd/attributes.h
@@ -13,19 +13,29 @@
 #ifndef INC_SIMD_ATTRIBUTES_H_
 #define INC_SIMD_ATTRIBUTES_H_
 
+#ifndef INLINE
 /* Set modifiers to always inline the code */
 #define INLINE static __attribute__((always_inline)) inline
+#endif
 
+#ifndef MALLOC
 /* malloc() function attribute */
 #define MALLOC __attribute__ ((__malloc__))
+#endif
 
+#ifndef NOTNULL
 /* Mark pointer parameters which must not be NULL */
 #define NOTNULL(...) __attribute__ ((__nonnull__ (__VA_ARGS__)))
+#endif
 
+#ifndef UNUSED
 /* Mark parameters as unused to avoid warnings */
 #define UNUSED __attribute__((unused))
+#endif
 
+#ifndef WARN_UNUSED_RESULT
 /* warn about unused result function attribute */
 #define WARN_UNUSED_RESULT __attribute__ ((__warn_unused_result__))
+#endif
 
 #endif  // INC_SIMD_ATTRIBUTES_H_
