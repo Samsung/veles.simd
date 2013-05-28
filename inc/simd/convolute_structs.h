20b7c8483d6d3bda0d1f480649c2dc9675216bd1
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-05-28 15:21:44 +0400

    Added missing convolute_structs.h
    Change-Id: Ifdd3722d45549130bda11142427b6957884f4a4a

---
+++ inc/simd/convolute_structs.h
@@ -0,0 +1,55 @@
+/*! @file convolute_structs.h
+ *  @brief New file description.
+ *  @author Markovtsev Vadim <v.markovtsev@samsung.com>
+ *  @version 1.0
+ *
+ *  @section Notes
+ *  This code partially conforms to <a href="http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml">Google C++ Style Guide</a>.
+ *
+ *  @section Copyright
+ *  Copyright 2013 Samsung R&D Institute Russia
+ */
+
+#ifndef INC_SIMD_CONVOLUTE_STRUCTS_H_
+#define INC_SIMD_CONVOLUTE_STRUCTS_H_
+
+#include <stddef.h>
+
+struct ConvoluteOverlapSaveHandle {
+  void *fft_plan;
+  void *fft_inverse_plan;
+  float *fft_boiler_plate;
+  float *H;
+  size_t x_length;
+  size_t h_length;
+  int *L;
+  int reverse;
+};
+
+struct ConvoluteFFTHandle {
+  void *fft_plan;
+  void *fft_inverse_plan;
+  int *M;
+  int x_length;
+  int h_length;
+  float **inputs;
+  int reverse;
+};
+
+typedef enum {
+  kConvolutionAlgorithmBruteForce,
+  kConvolutionAlgorithmFFT,
+  kConvolutionAlgorithmOverlapSave
+} ConvolutionAlgorithm;
+
+struct ConvoluteHandle {
+  ConvolutionAlgorithm algorithm;
+  int x_length;
+  int h_length;
+  union {
+    struct ConvoluteFFTHandle fft;
+    struct ConvoluteOverlapSaveHandle os;
+  } handle;
+};
+
+#endif  // INC_SIMD_CONVOLUTE_STRUCTS_H_
