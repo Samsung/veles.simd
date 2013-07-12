bcdcc39e80e7553011a15a52843668e9c904c56e
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-07-12 14:00:34 +0400

    Added comments to matrix functions
    Change-Id: If410fb653db74590fa8f713cc795a10db571d139

--- inc/simd/matrix.h
+++ inc/simd/matrix.h
@@ -20,10 +20,35 @@
 extern "C" {
 #endif
 
+/// @brief Multiplies two matrices.
+/// @param simd Value which indicates whether to use SIMD acceleration or not.
+/// @param m1 The first matrix in row-major format.
+/// @param m2 The seconds matrix in row-major format.
+/// @param w1 The width of the first matrix (the number of columns).
+/// @param h1 The height of the first matrix (the number of rows).
+/// @param w2 The width of the second matrix (the number of columns).
+/// @param h2 The height of the second matrix (the number of rows).
+/// @param res The resulting matrix, of size w2 x h1.
+/// @pre w1 must be equal to h2.
 void matrix_multiply(int simd, const float *m1, const float *m2,
                      size_t w1, size_t h1, size_t w2, size_t h2,
                      float *res) NOTNULL(2,3,8);
 
+/// @brief Multiplies two matrices, the second one being stored transposed.
+/// @param simd Value which indicates whether to use SIMD acceleration or not.
+/// @param m1 The first matrix in row-major format.
+/// @param m2 The seconds matrix in row-major format.
+/// @param w1 The width of the first matrix (the number of columns).
+/// @param h1 The height of the first matrix (the number of rows).
+/// @param w2 The width of the second (transposed) matrix
+/// (the number of columns). This value equals to the number of rows
+/// in original matrix.
+/// @param h2 The height of the second (transposed) matrix
+/// (the number of rows). This value equals to the number of columns
+/// in original matrix.
+/// @param res The resulting matrix, of size h2 x h1.
+/// @pre w1 must be equal to w2.
+/// @note This function typically performs 10% faster than matrix_multiply().
 void matrix_multiply_transposed(int simd, const float *m1, const float *m2,
                                 size_t w1, size_t h1, size_t w2, size_t h2,
                                 float *res) NOTNULL(2,3,8);
