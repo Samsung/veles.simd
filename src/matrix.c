cebe9be0713181323c7a22e46468c8cd3750eba9
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-07-16 11:07:05 +0400

    Fixed build on x86-64 machines without AVX

--- src/matrix.c
+++ src/matrix.c
@@ -195,16 +195,17 @@ void matrix_multiply_transposed(int simd, const float *m1, const float *m2,
   assert(w1 > 0);
   assert(h1 > 0);
   assert(h2 > 0);
-  if (!simd) {
-    matrix_multiply_transposed_novec(m1, m2, w1, h1, w2, h2, res);
-  } else {
+  if (simd) {
 #ifdef __ARM_NEON__
     matrix_multiply_transposed_neon(m1, m2, w1, h1, w2, h2, res);
+  } else {
 #elif defined(__AVX__)
     matrix_multiply_transposed_avx(m1, m2, w1, h1, w2, h2, res);
+  } else {
 #else
-#error SIMD version of this function is not implemented for the selected target
+  } {
 #endif
+    matrix_multiply_transposed_novec(m1, m2, w1, h1, w2, h2, res);
   }
 }
 
