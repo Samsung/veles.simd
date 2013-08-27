771cebd38146eb6a5d22fb965dddbb59b6bc5948
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-08-27 15:09:35 +0400

    Blacklisted avx_mathfun.h and neon_mathfun.h from style check
    Change-Id: I8d644cada95ba38a19ff788d7d59cd6b772ffe6b

--- fullcheck_xml.sh
+++ fullcheck_xml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./simd/*" -not -name "config.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
+find \( -name "*.cc" -o -name "*.h" \) -not -path "./tests/google/*" -not -path "./inc/simd/avx_mathfun.h" -not -path "./inc/simd/neon_mathfun.h" -not -name "config.h" -print0 | xargs -0 ./cpplint.py --verbose=0 --filter=+,-build/include_alpha,-build/include_what_you_use,-whitespace/operators 2>&1 | ./cpplint_to_cppcheckxml.py 2> cpplint.xml
