d359b874e00fd045587a4196ca4c2081aefdd0d0
Vadim Markovtsev <v.markovtsev@samsung.com>
2013-11-21 13:10:10 +0400

    Improved instruction_set.h to use g++ 4.8 SSE/AVX vector lane extraction

--- inc/simd/instruction_set.h
+++ inc/simd/instruction_set.h
@@ -13,8 +13,6 @@
 #ifndef INC_SIMD_INSTRUCTION_SET_H_
 #define INC_SIMD_INSTRUCTION_SET_H_
 
-#include <simd/common.h>
-
 #ifdef __SSE3__
 #ifdef __AVX__
 #include <immintrin.h>
@@ -23,35 +21,48 @@
 #include <simd/avxintrin-emu.h>
 #define __AVX__
 #endif
-#ifdef __cplusplus
-
-SIMD_API_BEGIN
+#elif defined(__ARM_NEON__)
+#include <arm_neon.h>
+#endif
 
 #ifdef __AVX__
 
-float _mm256_get_ps(__m256 vector, int index) __attribute__((optimize(2)));
-
+#ifndef __xgetbv
 static __attribute__((always_inline)) inline unsigned long long __xgetbv() {
 #if defined(__GNUC__) && __GNUC_PREREQ(4, 4)
   unsigned int index = 0;
   unsigned int eax, edx;
   __asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
+#ifdef __cplusplus
+  return (static_cast<unsigned long long>(edx) << 32) | eax;
+#else
   return ((unsigned long long)edx << 32) | eax;
+#endif
 #else
   return 0;
 #endif
 }
+#endif  // __xgetbv
 
-#endif  // __AVX__
+#if defined(__cplusplus) && __GNUC__ == 4 && __GNUC_MINOR__ < 8
 
-SIMD_API_END
+__attribute__((optimize(2), always_inline)) inline float
+_mm256_get_ps(__m256 vector, int index) {
+  union __m128_buggy_gxx_up_to_4_7 {
+      __m256 v;
+      float e[8];
+  };
+  __m128_buggy_gxx_up_to_4_7 vfix;
+  vfix.v = vector;
+  return vfix.e[index];
+}
+
 #else
-#ifndef _mm256_get_ps_IMPLEMENTATION
+
 #define _mm256_get_ps(vec, index) __m256_get_ps(vec, index)
-#endif  // _mm256_get_ps_IMPLEMENTATION
-#endif  // __cplusplus
-#elif defined(__ARM_NEON__)
-#include <arm_neon.h>
+
+#endif  // defined(__cplusplus) && __GNUC__ == 4 && __GNUC_MINOR__ < 8
+
 #endif
 
 #endif  // INC_SIMD_INSTRUCTION_SET_H_
